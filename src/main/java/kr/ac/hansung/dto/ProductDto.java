885874859935a8d2246769a9432b2478bf46b52c
Gureum <67192959+huheyun@users.noreply.github.com>
2026-06-07 12:39:16 +0900

    feat: ADMIN 전용 상품 수정 기능 및 검증 추가

--- src/main/java/kr/ac/hansung/dto/ProductDto.java
+++ src/main/java/kr/ac/hansung/dto/ProductDto.java
@@ -1,5 +1,8 @@
 package kr.ac.hansung.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,8 +10,16 @@ import lombok.Setter;
 @Setter
 public class ProductDto {
 
+    @NotBlank(message = "상품명을 입력하세요")
     private String name;
-    private int price;
+
+    @NotNull(message = "가격을 입력하세요")
+    @Min(value = 0, message = "가격은 0 이상이어야 합니다")
+    private Integer price;
+
     private String description;
-    private int stock;
+
+    @NotNull(message = "재고를 입력하세요")
+    @Min(value = 0, message = "재고는 0 이상이어야 합니다")
+    private Integer stock;
 }
